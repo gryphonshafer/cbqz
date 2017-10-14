b44cf239ec1843dddc8b5c8f2bfe77d993a55ecc
Gryphon Shafer <gryphon@goldenguru.com>
2017-10-14 11:34:56 -0700

    Question sets stats display (in a to-be-CSSed table)

--- lib/CBQZ/Model/QuestionSet.pm
+++ lib/CBQZ/Model/QuestionSet.pm
@@ -37,6 +37,43 @@ sub get_questions {
     return $questions;
 }
 
+sub statistics {
+    my ($self) = @_;
+
+    my %types;
+    my $type_data;
+    for (
+        @{ $self->dq->sql(q{
+            SELECT book, chapter, type, COUNT(*) AS questions
+            FROM question
+            WHERE question_set_id = ?
+            GROUP BY book, chapter, type
+        })->run( $self->obj->id )->all({}) }
+    ) {
+        $types{ $_->{type} } = 1;
+        $type_data->{ $_->{book} }{ $_->{chapter} }{ $_->{type} } = $_->{questions};
+    }
+
+    my @types = sort keys %types;
+
+    return [
+        map {
+            my $this_type_data = $type_data->{ $_->{book} }{ $_->{chapter} };
+            $_->{types} = [ map { [ $_, $this_type_data->{$_} ] } @types ];
+            $_;
+        } @{ $self->dq->sql(q{
+            SELECT
+                book, chapter,
+                COUNT( DISTINCT verse ) AS verses,
+                COUNT(*) AS questions
+            FROM question
+            WHERE question_set_id = ?
+            GROUP BY book, chapter
+            ORDER BY book, chapter
+        })->run( $self->obj->id )->all({}) }
+    ];
+}
+
 __PACKAGE__->meta->make_immutable;
 
 1;
