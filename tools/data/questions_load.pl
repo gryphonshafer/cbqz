f1a6e01c6d4681a1c2065c2a4ba262c15c30d548
Gryphon Shafer <gryphon@goldenguru.com>
2017-11-05 08:39:03 -0800

    Add ability to delete an existing set prior to load or create a new set

--- tools/data/questions_load.pl
+++ tools/data/questions_load.pl
@@ -5,13 +5,26 @@ use Util::CommandLine qw( options pod2usage );
 use Text::CSV_XS 'csv';
 use CBQZ;
 
-my $settings = options( qw( set|s=s questions|q=s ) );
+my $settings = options( qw( set|s=s questions|q=s create|c=s user|u=s delete|d ) );
 pod2usage unless ( $settings->{set} and $settings->{questions} );
 
 my $dq = CBQZ->new->dq;
+
+if ( $settings->{create} ) {
+    my $user_id = $dq->sql('SELECT user_id FROM user WHERE name = ?')->run( $settings->{user} )->value;
+    die "Failed to find user $settings->{user}\n" unless ($user_id);
+
+    $dq->sql('INSERT INTO question_set ( name, user_id ) VALUES ( ?, ? )')->run(
+        $settings->{create},
+        $user_id,
+    )->value;
+}
+
 my $set_id = $dq->sql('SELECT question_set_id FROM question_set WHERE name = ?')
     ->run( $settings->{set} )->value;
 die "Set name $settings->{set} not found\n" unless ($set_id);
+
+$dq->sql('DELETE FROM question WHERE question_set_id = ?')->run($set_id) if ( $settings->{delete} );
 
 my $insert = $dq->sql(q{
     INSERT INTO question ( question_set_id, type, book, chapter, verse, question, answer )
@@ -29,6 +42,9 @@ questions_load.pl - Load questions data into the database as a new questions set
     questions_load.pl OPTIONS
         -s|set       QUESTIONS_SET_NAME
         -q|questions QUESTIONS_DATA_FILE
+        -c|create    NEW_QUESTIONS_SET_NAME
+        -u|user      USERNAME
+        -d|delete
         -h|help
         -m|man
 
