fcc36d0c94e0097513adde19475d6f13b4dad7eb
Gryphon Shafer <gryphon@goldenguru.com>
2017-11-08 10:03:25 -0800

    Code changes to support security improvements; closes #55

--- lib/CBQZ/Control/Editor.pm
+++ lib/CBQZ/Control/Editor.pm
@@ -13,10 +13,10 @@ sub path ($self) {
 
 sub data ($self) {
     my $cbqz_prefs = $self->decode_cookie('cbqz_prefs');
+    my $material   = CBQZ::Model::MaterialSet->new->load( $cbqz_prefs->{material_set_id} )->get_material;
 
-    my $material  = CBQZ::Model::MaterialSet->new->load( $cbqz_prefs->{material_set_id} )->get_material;
-    # TODO: prevent get_questions if user doesn't own the set
-    my $questions = CBQZ::Model::QuestionSet->new->load( $cbqz_prefs->{question_set_id} )->get_questions;
+    my $set = CBQZ::Model::QuestionSet->new->load( $cbqz_prefs->{question_set_id} );
+    my $questions = ( $set and $set->is_owned_by( $self->stash('user') ) ) ? $set->get_questions : {};
 
     return $self->render( json => {
         metadata => {
@@ -48,25 +48,37 @@ sub save ($self) {
     my $question   = $self->req_body_json;
     my $cbqz_prefs = $self->decode_cookie('cbqz_prefs');
 
+    my $success = 0;
     unless ( $question->{question_id} ) {
-        $question->{used}            = 0;
-        $question->{question_set_id} = $cbqz_prefs->{question_set_id};
+        if (
+            CBQZ::Model::QuestionSet->new->load(
+                $cbqz_prefs->{question_set_id}
+            )->is_owned_by( $self->stash('user') )
+        ) {
+            $question->{used}            = 0;
+            $question->{question_set_id} = $cbqz_prefs->{question_set_id};
 
-        # TODO: prevent save to a question_set_id not owned by the user
-        $question = CBQZ::Model::Question->new->create($question)->data;
+            $question = CBQZ::Model::Question->new->create($question)->data;
+            $success  = 1;
+        }
     }
     else {
-        # TODO: prevent if question_set_id not owned by the user
-        CBQZ::Model::Question->new->load( $question->{question_id} )->obj->update($question);
+        my $question_model = CBQZ::Model::Question->new->load( $question->{question_id} );
+        if ( $question_model and $question_model->is_owned_by( $self->stash('user') ) ) {
+            $question_model->obj->update($question);
+            $success = 1;
+        }
     }
 
-    return $self->render( json => { question => $question } );
+    return $self->render( json => { question => $question } ) if ($success);
 }
 
 sub delete ($self) {
-    # TODO: prevent if question_set_id not owned by the user
-    CBQZ::Model::Question->new->load( $self->req_body_json->{question_id} )->obj->delete;
-    return $self->render( json => { success => 1 } );
+    my $question = CBQZ::Model::Question->new->load( $self->req_body_json->{question_id} );
+    if ( $question and $question->is_owned_by( $self->stash('user') ) ) {
+        $question->obj->delete;
+        return $self->render( json => { success => 1 } );
+    }
 }
 
 1;
--- lib/CBQZ/Control/Main.pm
+++ lib/CBQZ/Control/Main.pm
@@ -123,20 +123,20 @@ sub question_set_create ($self) {
 }
 
 sub question_set_delete ($self) {
-    # TODO: prevent delete if user doesn't own the set
-    CBQZ::Model::QuestionSet->new->load( $self->req_body_json->{question_set_id} )->obj->delete;
-    return $self->render( json => { success => 1 } );
+    my $set = CBQZ::Model::QuestionSet->new->load( $self->req_body_json->{question_set_id} );
+    if ( $set and $set->is_owned_by( $self->stash('user') ) ) {
+        $set->obj->delete;
+        return $self->render( json => { success => 1 } );
+    }
 }
 
 sub question_set_rename ($self) {
     my $data = $self->req_body_json;
-
-    # TODO: prevent rename if user doesn't own the set
-    my $set = CBQZ::Model::QuestionSet->new->load( $data->{question_set_id} )->obj;
-    $set->name( $data->{name} );
-    $set->update;
-
-    return $self->render( json => { success => 1 } );
+    my $set  = CBQZ::Model::QuestionSet->new->load( $data->{question_set_id} );
+    if ( $set and $set->is_owned_by( $self->stash('user') ) ) {
+        $set->obj->update({ name => $data->{name} });
+        return $self->render( json => { success => 1 } );
+    }
 }
 
 1;
