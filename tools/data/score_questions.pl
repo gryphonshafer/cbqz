0b263e25867bd2252d5b365d2b908514ccdefac2
Gryphon Shafer <gryphon@goldenguru.com>
2018-02-25 15:56:03 -0800

    Add "all" ability for question scoring

--- tools/data/score_questions.pl
+++ tools/data/score_questions.pl
@@ -7,26 +7,15 @@ use CBQZ::Model::Question;
 use CBQZ::Model::QuestionSet;
 use CBQZ::Model::MaterialSet;
 
-my $settings = options( qw( user|u=s questions|q=s materials|m=s ) );
-pod2usage unless ( $settings->{user} and $settings->{questions} and $settings->{materials} );
+my $settings = options( qw( all|a user|u=s questions|q=s materials|m=s ) );
+pod2usage unless (
+    (
+        $settings->{all} or
+        ( $settings->{user} and $settings->{questions} )
+    ) and $settings->{materials}
+);
 
-my ( $question_set, $material_set );
-
-try {
-    $question_set = CBQZ::Model::QuestionSet->new->load(
-        {
-            'me.name'       => $settings->{questions},
-            'user.username' => $settings->{user},
-        },
-        {
-            join => 'user',
-        }
-    );
-}
-catch {
-    die "Unable to load question set\n";
-};
-
+my $material_set;
 try {
     $material_set = CBQZ::Model::MaterialSet->new->load( { 'name' => $settings->{materials} } );
 }
@@ -34,8 +23,30 @@ catch {
     die "Unable to load material set\n";
 };
 
+my $question_set;
+unless ( $settings->{all} ) {
+    try {
+        $question_set = CBQZ::Model::QuestionSet->new->load(
+            {
+                'me.name'       => $settings->{questions},
+                'user.username' => $settings->{user},
+            },
+            {
+                join => 'user',
+            }
+        );
+    }
+    catch {
+        die "Unable to load question set\n";
+    };
+}
+
 $| = 1;
-for my $question ( CBQZ::Model::Question->new->model( $question_set->obj->questions->all ) ) {
+for my $question ( CBQZ::Model::Question->new->model(
+    ( $settings->{all} )
+        ? CBQZ::Model::Question->new->rs->search->all
+        : $question_set->obj->questions->all
+) ) {
     $question->calculate_score($material_set);
     print '.';
 }
@@ -50,10 +61,16 @@ score_questions.pl - Calculate and save question score for a question set.
     score_questions.pl OPTIONS
         -u|user       USERNAME
         -q|questions  QUESTION_SET_NAME
+        -a|all
         -m|materials  MATERIAL_SET_NAME
         -h|help
         -m|man
 
 =head1 DESCRIPTION
 
-This program will calculate and save question score for a question set.
+This program will calculate and save question score for a question set. It
+requires either a question set name and username or the "all" flag to be set,
+indicating all questions in the database. It also needs a material set named.
+
+    ./score_questions.pl -u username -q 'Questions' -m 'Materials'
+    ./score_questions.pl -a -m 'Materials'
