b7f4eefcc14c03195afc3479a6a661fe798276f0
Gryphon Shafer <gryphon@goldenguru.com>
2018-07-24 10:33:28 -0700

    Back-porting some SQL update scripts

--- db/changes/1528823624_lc_enums/deploy.sql
+++ db/changes/1528823624_lc_enums/deploy.sql
@@ -1,24 +1,8 @@
 # dest.prereq: db/changes/1517966309_roles_naming_update
 # dest.prereq: db/changes/1519065946_publish_question_sets
 
-ALTER TABLE role CHANGE COLUMN type type ENUM(
-    'Administrator', 'Director', 'Official', 'User', 'administrator', 'director', 'official', 'user'
-) NOT NULL AFTER program_id;
+ALTER TABLE role CHANGE COLUMN type
+    type ENUM( 'administrator', 'director', 'official', 'user' ) NOT NULL AFTER program_id;
 
-UPDATE role SET type = LOWER(type);
-
-ALTER TABLE role CHANGE COLUMN type type ENUM(
-    'administrator', 'director', 'official', 'user'
-) NOT NULL AFTER program_id;
-
-#-------------------------------------------------------------------------------
-
-ALTER TABLE user_question_set CHANGE COLUMN type type ENUM(
-    'Publish', 'Share', 'publish', 'share'
-) NOT NULL AFTER question_set_id;
-
-UPDATE user_question_set SET type = LOWER(type);
-
-ALTER TABLE user_question_set CHANGE COLUMN type type ENUM(
-    'publish', 'share'
-) NOT NULL AFTER question_set_id;
+ALTER TABLE user_question_set CHANGE COLUMN type
+    type ENUM( 'publish', 'share' ) NOT NULL AFTER question_set_id;
